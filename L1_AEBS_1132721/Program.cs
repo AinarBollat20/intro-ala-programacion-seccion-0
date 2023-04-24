85ee94c06e5bafcdee964210605fc8d409c98d5e
AinarBollat20 <123256286+AinarBollat20@users.noreply.github.com>
2023-04-23 20:48:57 -0600

    Add files via upload

---
+++ L1_AEBS_1132721/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su nombre:");
+        string Nombre = Console.ReadLine();
+
+        Console.WriteLine("Hola Mundo,");
+        Console.WriteLine("soy" +  Nombre);
+
+        /*COMENTARIOS*/
+
+        Console.Write("Hola mundo,");
+        Console.Write("soy"+  Nombre);
+        Console.ReadKey();
+
+        
+
+        Console.WriteLine("Mi segundo programa");
+
+        Console.WriteLine("Ingrese Nombre:");
+            string sNombre = Console.ReadLine();
+        Console.WriteLine("Ingrese Edad:");
+        string sEdad = Console.ReadLine();
+        Console.WriteLine("Ingrese Carrera:");
+        string sCarrera = Console.ReadLine();
+        Console.WriteLine("Ingrese Carne:");
+        string sCarne = Console.ReadLine();
+
+        Console.WriteLine("nombre:" + Nombre);
+        Console.WriteLine("edad:" + sEdad);
+        Console.WriteLine("carrera:" + sCarrera);
+        Console.WriteLine("carne:" + sCarne);
+        Console.ReadKey();
+        
+
+    }
+}
