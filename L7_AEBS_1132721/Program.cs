d8efd1971fd82ecb833ca38e333460158c064080
AinarBollat20 <123256286+AinarBollat20@users.noreply.github.com>
2023-04-23 21:31:03 -0600

    Add files via upload

---
+++ L7_AEBS_1132721/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_AEBS_1132721
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero mayor que 0: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+            int i = 0;
+
+            Console.Write("Sucesión de Fibonacci: ");
+
+            do
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+                i++;
+            } while (i < n);
+
+            Console.ReadLine();
+        }
+    }
+   
+
+
+
+
+
+        }
+    
+
