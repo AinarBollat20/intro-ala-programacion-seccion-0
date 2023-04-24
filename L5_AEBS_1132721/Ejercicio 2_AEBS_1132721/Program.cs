efdd690202a781df8d6f01c5fdb8e39aed6e6880
AinarBollat20 <123256286+AinarBollat20@users.noreply.github.com>
2023-04-23 20:57:47 -0600

    Add files via upload

---
+++ L5_AEBS_1132721/Ejercicio 2_AEBS_1132721/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 2 Ainar Estuardo Bollat Sam ");
+        Console.Write("Ingrese el numero de dia: ");
+        int num = int.Parse(Console.ReadLine());
+
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miércoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            case 6:
+                Console.WriteLine("Sábado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("Error: El número a ingresar debe estar contenido entre 1 y 7");
+                break;
+        }
+    }
+}
---
+++ L5_AEBS_1132721/Ejercicio No.1_Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio No.1...");
+        Console.WriteLine("Ingrese un numero:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        ;
+        switch (x)
+        {
+            case 1:
+                 x < 0;
+                Console.WriteLine("El numero es Negativo");
+                break;
+            case 2:
+                x = 0;
+                    Console.WriteLine("El Numero es 0 ");
+                break;
+            case 3:
+                 x > 0;
+                Console.WriteLine("El numero es positivo");
+                break;
+
+            default:
+                Console.WriteLine("Ingresaste una letra :( ");
+                break;
+
+
+
+        }
+    }
+    }
+            
